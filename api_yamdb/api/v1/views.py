4e591e7d60a38cf76ef03182211ce2b93c1495e0
Anatoly Solovov <anatolysolovov@gmail.com>
2023-02-07 00:52:56 +0300

    lookup

--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -23,6 +23,7 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsAdmin,)
+    lookup_field = 'username'
 
     @action(
         methods=[
