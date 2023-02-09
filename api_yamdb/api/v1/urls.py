a24195f195640cd596a03d207e5229935b9d061c
Vladimir Dmitriev <gdvsbook@gmail.com>
2023-02-10 01:19:08 +0300

    Выполнены все задания

--- api_yamdb/api/v1/urls.py
+++ api_yamdb/api/v1/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (TitleViewSet, CategoryViewSet, 
+from .views import (TitleViewSet, CategoryViewSet,
                     GenreViewSet, ReviewViewSet, CommentViewSet, UserViewSet,
                     SignupAPIView, TokenAPIView)
 
